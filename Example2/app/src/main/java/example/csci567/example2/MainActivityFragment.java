60e12a9d6b8879f0eda77eae84789b91226d88b7
Bryan Dixon <javawolfpack@gmail.com>
2017-02-07 15:08:14 -0800

    Example 2 updated

--- Example2/app/src/main/java/example/csci567/example2/MainActivityFragment.java
+++ Example2/app/src/main/java/example/csci567/example2/MainActivityFragment.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnLongClick;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,6 +21,20 @@ import butterknife.ButterKnife;
 public class MainActivityFragment extends Fragment {
 
     @BindView(R.id.tv3) TextView tv3;
+    @BindView(R.id.tv1)
+    EditText tv1;
+
+    @OnClick(R.id.button1)
+    public void clicked(View view) {
+        Toast.makeText(getContext(),tv1.getText(),Toast.LENGTH_SHORT).show();
+        // TODO submit data to server...
+    }
+
+    @OnLongClick(R.id.button1)
+    public boolean longclicked(View view){
+        Toast.makeText(getContext(),"Button Long Clicked", Toast.LENGTH_LONG).show();
+        return true;
+    }
 
     public MainActivityFragment() {
     }
@@ -25,6 +44,13 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.bind(this, view);
+//        Button button = (Button) view.findViewById(R.id.button1);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getContext(),"Button Clicked",Toast.LENGTH_SHORT).show();
+//            }
+//        });
         tv3.setText("Goodbye world");
         return view;
     }
